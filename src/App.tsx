01707e956df157a859d758d79cbcd777cd84c42f
Eytan3000 <120247900+Eytan3000@users.noreply.github.com>
2024-04-21 19:30:03 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import './App.css';
+import sound_321go from './assets/321done.mp3';
+function playSound() {
+  const audio = new Audio(sound_321go);
+  audio.play();
+}
+
+function App() {
+  const [restart, setrestart] = useState(0);
+  const [isPlaying, setisPlaying] = useState(false);
+
+  function play() {
+    playSound();
+  }
+
+  function handleOnUpdate(sec: number) {
+    sec === 3 && play();
+  }
+
+  return (
+    <>
+    <button onClick={()=>play()}>Play</button>
+      <div
+        onClick={() => {
+          setisPlaying((prev) => !prev);
+          setrestart((prev) => prev + 1);
+        }}>
+        <CountdownCircleTimer
+          // onComplete={() => {
+          //   // do your stuff here
+          //   return { shouldRepeat: true, delay: 1.5 }; // repeat animation in 1.5 seconds
+          // }}
+          key={restart}
+          isPlaying={isPlaying}
+          duration={60}
+          colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+          colorsTime={[7, 5, 2, 0]}
+          onUpdate={handleOnUpdate}>
+          {({ remainingTime }) => remainingTime}
+        </CountdownCircleTimer>
+      </div>
+    </>
+  );
+}
+
+export default App;
