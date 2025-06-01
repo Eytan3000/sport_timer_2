312f086ade8905b2db857c4ec254fc8f02cf4029
Eytan3000 <eytankrief@gmail.com>
2025-06-01 17:56:44 +0300

    type issues for deployment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,7 +15,7 @@
 // }
 
 
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
